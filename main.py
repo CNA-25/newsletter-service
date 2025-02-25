4ad4177d1bb2a662ce5ab1a14e36388a4f14c738
v3ngar <66013021+v3ngar@users.noreply.github.com>
2025-02-26 00:00:44 +0200

    Fixes #11

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ app = FastAPI()
 app.include_router(router)
 
 if __name__ == "__main__":
+    import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
 
 #Tillåter request från Users API
