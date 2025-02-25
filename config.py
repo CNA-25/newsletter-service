ee389b90a18319a230dc41e6c3d5ea683fac712e
v3ngar <66013021+v3ngar@users.noreply.github.com>
2025-02-26 00:54:53 +0200

    Updated Config for New Rahti build to accept Secrets

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
-from dotenv import load_dotenv
-from secret import JWT_SECRET, USERS_API_URL, PRODUCTS_API_URL, EMAIL_API_URL
 
-
-
+JWT_SECRET = os.getenv("JWT_SECRET")
+EMAIL_API_URL = os.getenv("EMAIL_API_URL")
+PRODUCTS_API_URL = os.getenv("PRODUCTS_API_URL")
+USERS_API_URL = os.getenv("USERS_API_URL")
